b93aa8cba3d6bd6cd4e429d714c53574b4fbaefd
Mukesh Otwani <mukeshotwani@Mukeshs-MacBook-Air-2.local>
2023-06-21 09:02:28 +0530

    new added statement

--- src/test/java/com/learnautomatiom/testcases/SmokeTest.java
+++ src/test/java/com/learnautomatiom/testcases/SmokeTest.java
@@ -11,8 +11,12 @@ public class SmokeTest extends BaseClass
 	@Test
 	public void verifyURL()
 	{
+		System.out.println("Test Started");
+		
 		Assert.assertTrue(driver.getCurrentUrl().contains("login"),"URL does not contain login");
 		
+		System.out.println("Test Ended");
+	
 	}
 	
 	
