8ff3e34321fb97181988bfdda530fc3a2d520862
OTS <131981730+selenium3bymukesh@users.noreply.github.com>
2023-06-21 09:01:53 +0530

    Update SmokeTest.java

--- src/test/java/com/learnautomatiom/testcases/SmokeTest.java
+++ src/test/java/com/learnautomatiom/testcases/SmokeTest.java
@@ -11,8 +11,11 @@ public class SmokeTest extends BaseClass
 	@Test
 	public void verifyURL()
 	{
+		System.out.println("Test Suite Started");
+		System.out.println("Test Case Started");
 		Assert.assertTrue(driver.getCurrentUrl().contains("login"),"URL does not contain login");
-		
+		System.out.println("Test Suite Completed");
+		System.out.println("Test Ended");
 	}
 	
 	
